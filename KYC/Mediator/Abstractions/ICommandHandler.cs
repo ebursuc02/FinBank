00ee6ce94249127803a7ee93744a7abea8b52db5
Asmarandei Catalin <asmarandei.catalin@yahoo.com>
2025-11-25 13:43:09 +0200

    [KYC] mediator integration and ports change

--- KYC/Mediator/Abstractions/ICommandHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace Mediator.Abstractions;
-
-public interface ICommandHandler<in TCommand, TResult> where TCommand : ICommand<TResult>
-{
-    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken);
-}
